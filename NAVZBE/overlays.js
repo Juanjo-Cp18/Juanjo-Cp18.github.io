77f994540ffbdab94ce2ef3d411420b9e36b103e
Juanjo-Cp18 <juanjo.vivasm@gmail.com>
2026-02-20 23:05:41 +0100

    Add files via upload

--- NAVZBE/overlays.js
+++ NAVZBE/overlays.js
@@ -1,4 +1,18 @@
 // Título: Configuración de Capas Visuales (Flechas)
-// Este archivo contiene las flechas verdes que tapan las flechas de OpenStreetMap.
+// Fecha: 20/2/2026, 23:03:44
+// Descarga este archivo al directorio de tu proyecto (reemplazando el anterior) para guardar los cambios.
 
-const PRELOADED_OVERLAYS = [];
+const PRELOADED_OVERLAYS = [
+    {
+        "id": 1771625007733,
+        "lat": 39.76845646938806,
+        "lng": 2.723773864959887,
+        "angle": 0
+    },
+    {
+        "id": 1771625016572,
+        "lat": 39.76845646938806,
+        "lng": 2.7235807081876295,
+        "angle": 183
+    }
+];
